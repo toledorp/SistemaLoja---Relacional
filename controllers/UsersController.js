3064fb7f692228d0d0d5bb50acc33a9ffa49a9a4
toledorp <roge.pupo.toledo@gmail.com>
2025-10-23 19:24:40 -0300

    cadastro de usuario

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,15 +1,35 @@
 import express from "express";
-const router = express.Router()
+const router = express.Router();
+
+//Importandeo models do Usuario
+import User from "../models/User.js";
 
 //ROTA DE LOGIN
-router.get("/login", (req, res) =>{
-    res.render("login")
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 //ROTA DE CADASTRO
-router.get("/cadastro", (req,res) =>{
-    res.render("cadastro")
+router.get("/cadastro", (req, res) => {
+  res.render("cadastro");
+});
+
+//ROTA DE CRIAÇÃO DE USUARIO
+router.post("/createUser", (req, res) => {
+  //Coletando os dados do formulario
+  const email = req.body.email;
+  const password = req.body.password;
+  //enviando para o banco
+  User.create({
+    email: email,
+    password: password,
+  })
+    .then(() => {
+      res.redirect("/login");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 export default router;
-
---
+++ models/User.js
@@ -0,0 +1,15 @@
+import Sequelize from "sequelize";
+import connection from "../config/sequelize-config.js";
+
+const User = connection.define("users", {
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    password:{
+        type: Sequelize.STRING,
+        allowNul: false,
+    },
+});
+User.sync({force: false})
+export default User;
